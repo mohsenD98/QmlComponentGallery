4cf103903849ae5ef14e12266ad8ada63bbdc218
mohsen <mohsen_d98@yahoo.com>
2021-08-13 13:34:01 +0430

    add images

--- QmlGallery/main.qml
+++ QmlGallery/main.qml
@@ -5,5 +5,19 @@ Window {
     visible: true
     width: 480
     height: 720
-    title: qsTr("Hello World")
+    title: qsTr("Qml Gallery")
+    color: materialColors.grey_100_
+
+    Colors{
+        id:materialColors
+    }
+
+    Rectangle{
+        id:toolbar
+        anchors.top: parent.top
+        anchors.right: parent.right
+        anchors.left: parent.left
+        height: 50
+        color: materialColors.grey_100_
+    }
 }
--- build-QmlGallery-Desktop_Qt_5_14_2_GCC_64bit-Profile/qmlcache_loader.cpp
+++ build-QmlGallery-Desktop_Qt_5_14_2_GCC_64bit-Profile/qmlcache_loader.cpp
@@ -4,18 +4,27 @@
 
 static const unsigned char qt_resource_tree[] = {
 0,
-0,0,0,0,2,0,0,0,1,0,0,0,1,0,0,0,
-8,0,0,0,0,0,1,0,0,0,0};
+0,0,0,0,2,0,0,0,2,0,0,0,1,0,0,0,
+8,0,0,0,0,0,1,0,0,0,0,0,0,0,34,0,
+0,0,0,0,1,0,0,0,0};
 static const unsigned char qt_resource_names[] = {
 0,
-1,0,0,0,47,0,47,0,8,8,1,90,92,0,109,0,
-97,0,105,0,110,0,46,0,113,0,109,0,108};
+1,0,0,0,47,0,47,0,10,6,159,28,92,0,67,0,
+111,0,108,0,111,0,114,0,115,0,46,0,113,0,109,0,
+108,0,8,8,1,90,92,0,109,0,97,0,105,0,110,0,
+46,0,113,0,109,0,108};
 static const unsigned char qt_resource_empty_payout[] = { 0, 0, 0, 0, 0 };
 QT_BEGIN_NAMESPACE
 extern Q_CORE_EXPORT bool qRegisterResourceData(int, const unsigned char *, const unsigned char *, const unsigned char *);
 QT_END_NAMESPACE
 namespace QmlCacheGeneratedCode {
 namespace _0x5f__main_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__Colors_qml { 
     extern const unsigned char qmlData[];
     const QQmlPrivate::CachedQmlUnit unit = {
         reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
@@ -36,6 +45,7 @@ Q_GLOBAL_STATIC(Registry, unitRegistry)
 
 Registry::Registry() {
         resourcePathToCachedUnit.insert(QStringLiteral("/main.qml"), &QmlCacheGeneratedCode::_0x5f__main_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/Colors.qml"), &QmlCacheGeneratedCode::_0x5f__Colors_qml::unit);
     QQmlPrivate::RegisterQmlUnitCacheHook registration;
     registration.version = 0;
     registration.lookupCachedQmlUnit = &lookupCachedUnit;
